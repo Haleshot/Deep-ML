ae68916c4b75ac1c1fb2d39733c8f1e4de4cfe48
Srihari Thyagarajan <hari.leo03@gmail.com>
2024-08-02 15:33:18 +0530

    Update logic

--- easy/scalar_multiplication_of_a_matrix/scalar_multiplication_of_a_matrix.py
+++ easy/scalar_multiplication_of_a_matrix/scalar_multiplication_of_a_matrix.py
@@ -1,9 +1,13 @@
 def scalar_multiply(matrix: list[list[int|float]], scalar: int|float) -> list[list[int|float]]:
-    return [[j * scalar] for i in matrix for j in i]
+    l = []
+    for i in matrix:
+        l.append([j * scalar for j in i])
+    return l
 
 matrix = [[1, 2], [3, 4]]
 scalar = 2
 print(scalar_multiply(matrix=matrix, scalar=scalar))
+
 
 # import numpy as np
 
