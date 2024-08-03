33b778815ce22baf518bb56cb65d14c60ba3de00
Srihari Thyagarajan <hari.leo03@gmail.com>
2024-08-03 09:38:17 +0530

    Logic; add multiple steps

---
+++ easy/transpose_of_a_matrix/transpose_of_a_matrix.py
@@ -0,0 +1,26 @@
+# # Step 1
+def transpose_matrix(a : list[list[int | float]]) -> list[list[int | float]]:
+    l = [[a[j][i] for j in range(len(a))] for i in range(len(a[0]))]
+    return l
+
+a = [[1, 2, 3], [4, 5, 6]] 
+print(transpose_matrix(a=a))
+
+
+# # Step 2
+# def transpose_matrix(a : list[list[int | float]]) -> list[list[int | float]]:
+#     matrix_a = list(zip(*a))
+#     return [list(a) for a in matrix_a]
+
+# a = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] 
+# print(transpose_matrix(a=a))
+
+
+# # Step 3
+# import numpy as np
+
+# def transpose_matrix(a : list[list[int | float]]) -> list[list[int | float]]:
+#     return np.matrix_transpose(a)
+
+# a = [[1, 2, 3], [4, 5, 6]]
+# print(transpose_matrix(a=a))
