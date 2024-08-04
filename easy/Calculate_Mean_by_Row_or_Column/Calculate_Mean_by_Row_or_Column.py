8903da4859c3dddef473a7cdff4cdc7814c85db8
Srihari Thyagarajan <hari.leo03@gmail.com>
2024-08-04 22:17:49 +0530

    Update logic

--- easy/Calculate_Mean_by_Row_or_Column/Calculate_Mean_by_Row_or_Column.py
+++ easy/Calculate_Mean_by_Row_or_Column/Calculate_Mean_by_Row_or_Column.py
@@ -5,16 +5,14 @@ def calculate_matrix_mean(matrix: list[list[float]], mode: str) -> list[float]:
         sum = 0
         for i in matrix:
             sum += i[0]
-            # sum = sum / len(matrix)
 
     elif mode == "column":
         sum = 0
-        for i in matrix:
-            print(i)
-            sum += i[0]
+        for k in range(len(matrix)):
+            for i in matrix:
+                sum += i[0]
             l.append(sum)
-            # sum = sum / len(matrix[0])
-    return sum
+    return l
 
 matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 mode = "column"
