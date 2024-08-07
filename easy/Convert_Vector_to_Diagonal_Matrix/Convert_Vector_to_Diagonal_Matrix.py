cbdb3e846234938b529ad763d610d8ff570a1e42
Srihari Thyagarajan <hari.leo03@gmail.com>
2024-08-08 00:24:35 +0530

    Add step 2

--- easy/Convert_Vector_to_Diagonal_Matrix/Convert_Vector_to_Diagonal_Matrix.py
+++ easy/Convert_Vector_to_Diagonal_Matrix/Convert_Vector_to_Diagonal_Matrix.py
@@ -1,13 +1,21 @@
 import numpy as np
 
+# # Step 1:
 def make_diagonal(x : list[int | float]) -> list[list[int | float]]:
     l = [[0] * len(x)] * len(x)
-    for i in range(len(x)):
-        for j in  range(len(x)):
-            if i == j:
-                l[i][j] = (x[j])
-                
+        # for i in range(len(x)):
+        #     for j in  range(len(x)):
+        #         if i == j:
+        #             l[i][j] = x[i]
+        #             print(l[i][j])
+        #         l = [i for i in l if i not in ] 
     return l
+
+
+
+# # Step 2
+# def make_diagonal(x : list[int | float]) -> list[list[int | float]]:
+#     return np.identity(np.size(x)) * x
 
 
 x = np.array([1, 2, 3])
