ced8b29fcef980b82a9f2d3a704c3b5523ba1b48
Srihari Thyagarajan <hari.leo03@gmail.com>
2024-08-08 08:20:42 +0530

    Add logic

--- easy/Convert_Vector_to_Diagonal_Matrix/Convert_Vector_to_Diagonal_Matrix.py
+++ easy/Convert_Vector_to_Diagonal_Matrix/Convert_Vector_to_Diagonal_Matrix.py
@@ -1,18 +1,20 @@
 import numpy as np
 
-# Step 1:
-def make_diagonal(x : list[int | float]) -> list[list[int | float]]:
-    l = [[0] * len(x) for _ in range(len(x))]
-    for i in range(len(x)):
-        l[i][i] = x[i]
-    return np.array(l).tolist()
-
+# # Step 1:
+def reshape_matrix(a: list[list[int|float]], new_shape: tuple[int, int]) -> list[list[int|float]]:
+    l = list(zip(*a))
+    print(new_shape[0], "\t", new_shape[1])
+    # for i in range(new_shape[1]):
+    #     for j in range(new_shape[0]):
+    #         print(j)
+    # l = [a[j][i] for j in range(new_shape[0]) for i in range(new_shape[1])]
+    return l
 
 # # Step 2
-# def make_diagonal(x : list[int | float]) -> list[list[int | float]]:
-#     return np.identity(np.size(x)) * x
+# def reshape_matrix(a: list[list[int|float]], new_shape: tuple[int, int]) -> list[list[int|float]]:
+#     # Write your code here and return a python list after reshaping by using numpy's tolist() method
+#     return np.reshape(a, newshape=new_shape).tolist()
 
-
-x = np.array([1, 2, 3])
-output = make_diagonal(x)
-print(output)
+a = [[1,2,3,4],[5,6,7,8]]
+new_shape = (4, 2)
+print(reshape_matrix(a=a, new_shape=new_shape))
