9f939ccb92f47e1b6ff9176a557a77a04af8b882
Srihari Thyagarajan <hari.leo03@gmail.com>
2024-08-08 22:07:28 +0530

    Init commit

--- easy/Transformation_Matrix_from_Basis_B_to_C/Transformation_Matrix_from_Basis_B_to_C.py
+++ easy/Transformation_Matrix_from_Basis_B_to_C/Transformation_Matrix_from_Basis_B_to_C.py
@@ -1,2 +1,14 @@
 def transform_basis(B: list[list[int]], C: list[list[int]]) -> list[list[float]]:
     pass
+
+
+
+B = [[1, 0, 0], 
+    [0, 1, 0], 
+    [0, 0, 1]]
+
+C = [[1, 2.3, 3], 
+    [4.4, 25, 6], 
+    [7.4, 8, 9]]
+
+print(transform_basis(B=B, C=C))
