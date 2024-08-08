1a425b9bab8f3b6c28be326eade9ba29c55e6070
Srihari Thyagarajan <hari.leo03@gmail.com>
2024-08-08 08:29:21 +0530

    Add logic

---
+++ easy/Reshape_Matrix/Reshape_Matrix.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+# # Step 1:
+def reshape_matrix(a: list[list[int|float]], new_shape: tuple[int, int]) -> list[list[int|float]]:
+    l = [[0] * new_shape[1] for _ in range(new_shape[0])]
+    print(l)
+    for i in range(new_shape[1]):
+        for j in range(new_shape[0]):
+            print(a[i][j])
+            l.append(a[i][j])
+            
+        l = [i for i in l if i not in a]
+    # l = [a[j][i] for j in range(new_shape[0]) for i in range(new_shape[1])]
+
+    return l
+
+# # Step 2
+# def reshape_matrix(a: list[list[int|float]], new_shape: tuple[int, int]) -> list[list[int|float]]:
+#     # Write your code here and return a python list after reshaping by using numpy's tolist() method
+#     return np.reshape(a, newshape=new_shape).tolist()
+
+a = [[1, 2, 3, 4], [5, 6, 7, 8]]
+new_shape = (4, 2)
+print(reshape_matrix(a=a, new_shape=new_shape))
