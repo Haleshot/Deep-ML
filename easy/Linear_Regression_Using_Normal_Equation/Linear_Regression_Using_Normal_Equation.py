f88035d6cd43e6b4813fec1bb2e05363e907fe73
Srihari Thyagarajan <hari.leo03@gmail.com>
2024-08-12 08:04:58 +0530

    Add logic

--- easy/Linear_Regression_Using_Normal_Equation/Linear_Regression_Using_Normal_Equation.py
+++ easy/Linear_Regression_Using_Normal_Equation/Linear_Regression_Using_Normal_Equation.py
@@ -2,4 +2,12 @@ import numpy as np
 
 def linear_regression_normal_equation(X: list[list[float]], y: list[float]) -> list[float]:
     # Your code here, make sure to round
-    return theta
+    mat = np.dot(np.transpose(X), X)
+    mat = np.linalg.inv(mat)
+    mult = np.dot(np.transpose(X), y)
+    theta = np.dot(mat, mult)
+    return np.round(theta, 2)
+
+X = [[1, 1], [1, 2], [1, 3]]
+y = [1, 2, 3]
+print(linear_regression_normal_equation(X=X, y=y))
