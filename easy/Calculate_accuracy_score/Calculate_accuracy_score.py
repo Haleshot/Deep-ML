06bdc7e660c6f1965d5836c9c13758bd4a61d6f5
Srihari Thyagarajan <hari.leo03@gmail.com>
2024-08-13 08:21:59 +0530

    Add Logic

--- easy/Calculate_accuracy_score/Calculate_accuracy_score.py
+++ easy/Calculate_accuracy_score/Calculate_accuracy_score.py
@@ -1,8 +1,20 @@
 import numpy as np
 
+# Step 1
 def accuracy_score(y_true : list[int | float], y_pred : list[int | float]) -> float:
     # Your code here
-    return final_value
+    c = 0
+    for i in range(len(y_true)):
+        if y_true[i] == y_pred[i]:
+            c += 1
+    return c/len(y_true)
+
+# # Step 2:
+# def accuracy_score(y_true : list[int | float], y_pred : list[int | float]) -> float:
+#     # Your code here
+#     accuracy = np.sum(y_true == y_pred, axis=0)/len(y_pred)
+#     return accuracy
+
 
 y_true = np.array([1, 0, 1, 1, 0, 1])
 y_pred = np.array([1, 0, 0, 1, 0, 1])
