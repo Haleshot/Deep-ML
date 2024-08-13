0b69d32b53390983f8660e136e4b278dca2847a7
Srihari Thyagarajan <hari.leo03@gmail.com>
2024-08-13 07:59:09 +0530

    init commit

--- easy/Calculate_accuracy_score/Calculate_accuracy_score.py
+++ easy/Calculate_accuracy_score/Calculate_accuracy_score.py
@@ -1,5 +1,10 @@
 import numpy as np
 
-def accuracy_score(y_true, y_pred):
+def accuracy_score(y_true : list[int | float], y_pred : list[int | float]) -> float:
     # Your code here
-    pass
+    return final_value
+
+y_true = np.array([1, 0, 1, 1, 0, 1])
+y_pred = np.array([1, 0, 0, 1, 0, 1])
+output = accuracy_score(y_true, y_pred)
+print(output)
