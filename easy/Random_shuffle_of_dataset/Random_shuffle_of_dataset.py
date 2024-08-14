4e3787f1f2ca0ac24594e96f4316fa2996a9c247
Srihari Thyagarajan <hari.leo03@gmail.com>
2024-08-14 15:37:16 +0530

    Init commit

--- easy/Random_shuffle_of_dataset/Random_shuffle_of_dataset.py
+++ easy/Random_shuffle_of_dataset/Random_shuffle_of_dataset.py
@@ -3,3 +3,6 @@ import numpy as np
 def shuffle_data(X : list[list[int]], y : list[list[int]], seed=None):
     # Your code here
     pass
+
+X = np.array([[1, 2], [3, 4], [5, 6], [7, 8]])
+y = np.array([1, 2, 3, 4])
