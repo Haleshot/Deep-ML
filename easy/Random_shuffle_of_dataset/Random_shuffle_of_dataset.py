e6de2559ef92bed8a64b7cb33a1ccd9954b3c888
Srihari Thyagarajan <hari.leo03@gmail.com>
2024-08-14 22:38:00 +0530

    Add Logic

--- easy/Random_shuffle_of_dataset/Random_shuffle_of_dataset.py
+++ easy/Random_shuffle_of_dataset/Random_shuffle_of_dataset.py
@@ -2,16 +2,17 @@ import numpy as np
 import random
 
 # Step 1
-def shuffle_data(X : list[list[int]], y : list[int], seed=None):
+def shuffle_data(X: list[list[int]], y: list[int], seed=None):
     if seed:
-        np.random.seed(seed)
+        np.random.seed(seed)  # Set the random seed for reproducibility
+
     x_len = len(X)
-    indices = list(range(x_len))
+    indices = list(range(x_len))  # Create a list of indices from 0 to len(X)-1
+    
+    np.random.shuffle(indices)  # Shuffle the list of indices
 
-    random.shuffle(indices)
-
-    finalX = [X[i] for i in indices]
-    finaly = [y[i] for i in indices]
+    finalX = [X[i] for i in indices]  # Rearrange X according to the shuffled indices
+    finaly = [y[i] for i in indices]  # Rearrange y according to the shuffled indices
 
     return finalX, finaly
     
