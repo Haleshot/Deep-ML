018ecd6c0e7d642896188c765078e42c2337581e
Srihari Thyagarajan <hari.leo03@gmail.com>
2024-08-14 15:37:35 +0530

    Add Logic

--- easy/Random_shuffle_of_dataset/Random_shuffle_of_dataset.py
+++ easy/Random_shuffle_of_dataset/Random_shuffle_of_dataset.py
@@ -6,4 +6,4 @@ def shuffle_data(X : list[list[int]], y : list[list[int]], seed=None):
 
 X = np.array([[1, 2], [3, 4], [5, 6], [7, 8]])
 y = np.array([1, 2, 3, 4])
-print(shuffle_data(X=X, y=y))
+print(shuffle_data(X=X, y=y)) # type: ignore
