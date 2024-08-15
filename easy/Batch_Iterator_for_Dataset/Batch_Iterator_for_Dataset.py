7d1e1474da92d81fa8df7801446373790c3b2660
Srihari Thyagarajan <hari.leo03@gmail.com>
2024-08-15 20:57:16 +0530

    Init commit

--- easy/Batch_Iterator_for_Dataset/Batch_Iterator_for_Dataset.py
+++ easy/Batch_Iterator_for_Dataset/Batch_Iterator_for_Dataset.py
@@ -3,3 +3,12 @@ import numpy as np
 def batch_iterator(X, y=None, batch_size=64):
     # Your code here
     pass
+
+X = np.array([[1, 2], 
+                  [3, 4], 
+                  [5, 6], 
+                  [7, 8], 
+                  [9, 10]])
+y = np.array([1, 2, 3, 4, 5])
+batch_size = 2
+print(batch_iterator(X, y, batch_size))
