1d55168b70fbfec8ac38a53eb27a87c88f2cd9ca
Srihari Thyagarajan <hari.leo03@gmail.com>
2024-08-16 08:00:21 +0530

    Init commit

--- easy/Log_Softmax/Log_Softmax.py
+++ easy/Log_Softmax/Log_Softmax.py
@@ -1,5 +1,8 @@
 import numpy as np
 
-def log_softmax(scores: list) -> np.ndarray:
+def log_softmax(scores: list[int | float]) -> np.ndarray:
     # Your code here
     pass
+
+A = np.array([1, 2, 3])
+print(log_softmax(A))
