2fd44dcb37dfef02d31fd51793e72c78617f0a58
Srihari Thyagarajan <hari.leo03@gmail.com>
2024-08-17 08:53:05 +0530

    Update logic

--- easy/Sigmoid_Activation_Function_Understanding/Sigmoid_Activation_Function_Understanding.py
+++ easy/Sigmoid_Activation_Function_Understanding/Sigmoid_Activation_Function_Understanding.py
@@ -1,8 +1,10 @@
 import numpy as np
 
+
 def sigmoid(z: float) -> float:
     # Your code here
-    return round(1/(1 + np.exp(-z)), 4)
+    return round(1 / (1 + np.exp(-z)), 4)
+
 
 z = 0
 print(sigmoid(z=z))
