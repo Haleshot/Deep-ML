9f6bf32b30ea53790e61e0cf7cd9439caea3a489
Srihari Thyagarajan <hari.leo03@gmail.com>
2024-08-17 09:20:27 +0530

    Update import statements

--- easy/Softmax_Activation_Function_Implementation/Softmax_Activation_Function_Implementation.py
+++ easy/Softmax_Activation_Function_Implementation/Softmax_Activation_Function_Implementation.py
@@ -1,6 +1,19 @@
 import math
+import numpy as np
 
-
+# # Step 1
 def softmax(scores: list[int | float]) -> list[float]:
     # Your code here
+    sum_exp_scores = sum([math.exp(i) for i in scores])
+    probabilities = [round(math.exp(i)/sum_exp_scores, 4) for i in scores]
     return probabilities
+
+# # Step 2
+def softmax(scores: list[int | float]) -> list[float]:
+    # Your code here
+    sum_exp_scores = sum([np.exp(i) for i in scores])
+    probabilities = [round(np.exp(i)/sum_exp_scores, 4) for i in scores]
+    return probabilities
+
+scores = [1, 2, 3]
+print(softmax(scores=scores))
