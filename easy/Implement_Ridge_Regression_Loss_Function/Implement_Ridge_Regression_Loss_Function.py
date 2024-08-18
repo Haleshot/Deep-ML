98eeef22c028c2c37fd6135f2581ff1871c0b8fa
Srihari Thyagarajan <hari.leo03@gmail.com>
2024-08-18 20:40:26 +0530

    Update logic

--- easy/Implement_Ridge_Regression_Loss_Function/Implement_Ridge_Regression_Loss_Function.py
+++ easy/Implement_Ridge_Regression_Loss_Function/Implement_Ridge_Regression_Loss_Function.py
@@ -1,16 +1,6 @@
 import numpy as np
 
-# # Step 1
-# def ridge_loss(X: np.ndarray, w: np.ndarray, y_true: np.ndarray, alpha: float) -> float:  # type: ignore
-#     # Your code here
-#     y_pred = (X @ w).tolist()
-#     MSE = np.mean((y_true - y_pred)**2).tolist()
-#     reg = alpha * np.sum(w**2).tolist()
-#     # print(MSE, reg)
-#     return MSE + reg
-
-
-# # Step 2:
+# # Step 1:
 def ridge_loss(X: np.ndarray, w: np.ndarray, y_true: np.ndarray, alpha: float) -> float:
     y_pred = []
     for i in range(len(X)):
@@ -26,6 +16,17 @@ def ridge_loss(X: np.ndarray, w: np.ndarray, y_true: np.ndarray, alpha: float) -
     reg = alpha * sum_w
     # print(MSE, reg)
     return MSE + reg
+
+
+# # Step 2
+# def ridge_loss(X: np.ndarray, w: np.ndarray, y_true: np.ndarray, alpha: float) -> float:  # type: ignore
+#     # Your code here
+#     y_pred = (X @ w).tolist()
+#     MSE = np.mean((y_true - y_pred)**2).tolist()
+#     reg = alpha * np.sum(w**2).tolist()
+#     # print(MSE, reg)
+#     return MSE + reg
+
 
 
 X = np.array([[1, 1], [2, 1], [3, 1], [4, 1]])
