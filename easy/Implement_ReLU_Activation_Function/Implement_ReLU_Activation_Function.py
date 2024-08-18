68b54a7cd1bee7d674dcfe47c852aa147ba2af6a
Srihari Thyagarajan <hari.leo03@gmail.com>
2024-08-18 16:37:26 +0530

    Update logic

--- easy/Implement_ReLU_Activation_Function/Implement_ReLU_Activation_Function.py
+++ easy/Implement_ReLU_Activation_Function/Implement_ReLU_Activation_Function.py
@@ -1,10 +1,16 @@
+# # Step 1:
 def relu(z: float) -> float:
-    # Your code here
-    if z <= 0:
-        return 0
-    else:
-        return max(0, z)
+    return max(0, z)
 
 
-# # Testing
-# print(relu(2))
+# # Step 2
+# def relu(z: float) -> float:
+#     # Your code here
+#     if z <= 0:
+#         return 0
+#     else:
+#         return max(0, z)
+
+
+# Testing
+print(relu(2))
