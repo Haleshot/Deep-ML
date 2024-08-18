d5cf01a3d4ebc10d00482a355a188cc3405c5424
Srihari Thyagarajan <hari.leo03@gmail.com>
2024-08-18 19:41:14 +0530

    Init commit

--- easy/Implement_Ridge_Regression_Loss_Function/Implement_Ridge_Regression_Loss_Function.py
+++ easy/Implement_Ridge_Regression_Loss_Function/Implement_Ridge_Regression_Loss_Function.py
@@ -1,8 +1,10 @@
 import numpy as np
 
+
 def ridge_loss(X: np.ndarray, w: np.ndarray, y_true: np.ndarray, alpha: float) -> float:
     # Your code here
     pass
+
 
 X = np.array([[1, 1], [2, 1], [3, 1], [4, 1]])
 w = np.array([0.2, 2])
