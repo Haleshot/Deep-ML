0be695319d86b3a78c2c729f4a08e5c2cb699c13
Srihari Thyagarajan <hari.leo03@gmail.com>
2024-08-20 20:41:38 +0530

    Add logic

--- easy/Feature_Scaling_Implementation/Feature_Scaling_Implementation.py
+++ easy/Feature_Scaling_Implementation/Feature_Scaling_Implementation.py
@@ -4,13 +4,13 @@ import numpy as np
 def feature_scaling(data: np.ndarray) -> (np.ndarray, np.ndarray): # type: ignore
     # Your code here
     standardized_data, normalized_data = [[] for _ in range(len(data))], [[] for _ in range(len(data))]
-    j = 0
+    c = 0
+    print(np.std(data[0]))
     for i in data:
-        while j < len(data):
-            std_dev = np.std(data)
-            mu = np.mean(data)
-            standardized_data[j].append((data[j] - mu) / std_dev)
-            j += 1
+        std_dev = np.std(i)
+        mu = np.mean(i)
+        standardized_data[c].append((i - mu) / std_dev)
+        c += 1
     
     # for i in range(len(data)):
     #     std_dev = np.std(data[i])
