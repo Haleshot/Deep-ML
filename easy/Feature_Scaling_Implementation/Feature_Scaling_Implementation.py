a0211eac1ad2dbbaff78dcb4806e63bacda9c774
Srihari Thyagarajan <hari.leo03@gmail.com>
2024-08-20 20:05:27 +0530

    Init commit

--- easy/Feature_Scaling_Implementation/Feature_Scaling_Implementation.py
+++ easy/Feature_Scaling_Implementation/Feature_Scaling_Implementation.py
@@ -1,8 +1,10 @@
 import numpy as np
 
+
 def feature_scaling(data: np.ndarray) -> (np.ndarray, np.ndarray):
     # Your code here
     return standardized_data, normalized_data
+
 
 data = np.array([[1, 2], [3, 4], [5, 6]])
 print(feature_scaling(data=data))
