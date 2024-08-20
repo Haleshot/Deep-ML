9c4c664f40e9c3f02dcba4714999d06a4f539fdd
Srihari Thyagarajan <hari.leo03@gmail.com>
2024-08-20 20:41:47 +0530

    Format using `black`

--- easy/Feature_Scaling_Implementation/Feature_Scaling_Implementation.py
+++ easy/Feature_Scaling_Implementation/Feature_Scaling_Implementation.py
@@ -1,9 +1,11 @@
 import numpy as np
 
 
-def feature_scaling(data: np.ndarray) -> (np.ndarray, np.ndarray): # type: ignore
+def feature_scaling(data: np.ndarray) -> (np.ndarray, np.ndarray):  # type: ignore
     # Your code here
-    standardized_data, normalized_data = [[] for _ in range(len(data))], [[] for _ in range(len(data))]
+    standardized_data, normalized_data = [[] for _ in range(len(data))], [
+        [] for _ in range(len(data))
+    ]
     c = 0
     print(np.std(data[0]))
     for i in data:
@@ -11,14 +13,12 @@ def feature_scaling(data: np.ndarray) -> (np.ndarray, np.ndarray): # type: ignor
         mu = np.mean(i)
         standardized_data[c].append((i - mu) / std_dev)
         c += 1
-    
+
     # for i in range(len(data)):
     #     std_dev = np.std(data[i])
     #     mu = np.mean(data[i])
     #     standardized_data[i].append((data[i] - mu) / std_dev)
-    
-    
-        
+
     return standardized_data, normalized_data
 
 
