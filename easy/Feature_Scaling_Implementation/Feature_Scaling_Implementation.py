c562118fcf906fa71f086f61591e64b7bd1015e0
Srihari Thyagarajan <hari.leo03@gmail.com>
2024-08-21 21:31:52 +0530

    Add Step 2 - shortened version

--- easy/Feature_Scaling_Implementation/Feature_Scaling_Implementation.py
+++ easy/Feature_Scaling_Implementation/Feature_Scaling_Implementation.py
@@ -2,20 +2,33 @@ import numpy as np
 import math
 
 
-# Step 1
-def feature_scaling(data: np.ndarray) -> (np.ndarray, np.ndarray):  # type: ignore
+# # Step 1
+# def feature_scaling(data: np.ndarray) -> (np.ndarray, np.ndarray):  # type: ignore
+#     # Standardization
+
+#     std = np.std(data, axis=0)
+#     mean = np.mean(data, axis=0)
+#     print(std, mean)
+#     standardized_data = (data - mean) / std
+
+#     # MinMax normalization data
+#     min_val = np.min(data, axis=0)
+#     max_val = np.max(data, axis=0)
+#     print(min_val, max_val)
+#     normalized_data = (data - min_val) / (max_val - min_val)
+
+#     return (
+#         np.round(standardized_data, 4).tolist(),
+#         np.round(normalized_data, 4).tolist(),
+#     )
+
+# # Step 2
+def feature_scaling(data: np.ndarray) -> (np.ndarray, np.ndarray): # type: ignore
     # Standardization
+    standardized_data = (data - np.mean(data, axis=0)) / np.std(data, axis=0)
 
-    std = np.std(data, axis=0)
-    mean = np.mean(data, axis=0)
-    print(std, mean)
-    standardized_data = (data - mean) / std
-
-    # MinMax normalization data
-    min_val = np.min(data, axis=0)
-    max_val = np.max(data, axis=0)
-    print(min_val, max_val)
-    normalized_data = (data - min_val) / (max_val - min_val)
+    # MinMax normalization
+    normalized_data = (data - np.min(data, axis=0)) / (np.max(data, axis=0) - np.min(data, axis=0))
 
     return (
         np.round(standardized_data, 4).tolist(),
