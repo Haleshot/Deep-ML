bd9799ab58fa9b2eefd89596353ad5a5830ccc0c
Srihari Thyagarajan <hari.leo03@gmail.com>
2024-08-21 21:02:20 +0530

    Add logic

--- easy/Feature_Scaling_Implementation/Feature_Scaling_Implementation.py
+++ easy/Feature_Scaling_Implementation/Feature_Scaling_Implementation.py
@@ -11,8 +11,11 @@ def feature_scaling(data: np.ndarray) -> (np.ndarray, np.ndarray):  # type: igno
     standardized_data = (data - mean) / std
 
     # MinMax normalization data
+    min_val = np.min(data, axis=0)
+    max_val = np.max(data, axis=0)
+    normalized_data = (data - min_val) / (max_val - min_val)
 
-    return standardized_data, normalized_data
+    return np.round(standardized_data, 4).tolist(), np.round(normalized_data, 4).tolist()
 
 
 data = np.array([[1, 2], [3, 4], [5, 6]])
