31911a83bfd16509f6336e9b16c78cde7181d5a1
Srihari Thyagarajan <hari.leo03@gmail.com>
2024-08-22 21:14:24 +0530

    Format using `black`

--- easy/Leaky_ReLU_Activation_Function/Leaky_ReLU_Activation_Function.py
+++ easy/Leaky_ReLU_Activation_Function/Leaky_ReLU_Activation_Function.py
@@ -1,18 +1,19 @@
 # Step 1
-def leaky_relu(z: float, alpha: float = 0.01) -> float | int: # type: ignore
+def leaky_relu(z: float, alpha: float = 0.01) -> float | int:  # type: ignore
     # Your code here
     if z > 0:
         return z
     else:
         return alpha * z
 
-print(leaky_relu(0)) 
+
+print(leaky_relu(0))
 # Output: 0
 
-print(leaky_relu(1)) 
+print(leaky_relu(1))
 # Output: 1
 
-print(leaky_relu(-1)) 
+print(leaky_relu(-1))
 # Output: -0.01
 
 print(leaky_relu(-2, alpha=0.1))
