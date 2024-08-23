64fad7f28d00111fb7230080346c65e8dccacb90
Srihari Thyagarajan <hari.leo03@gmail.com>
2024-08-23 18:48:14 +0530

    init commit

--- easy/One_Hot_Encoding_of_Nominal_Values/One_Hot_Encoding_of_Nominal_Values.py
+++ easy/One_Hot_Encoding_of_Nominal_Values/One_Hot_Encoding_of_Nominal_Values.py
@@ -1,5 +1,11 @@
 import numpy as np
 
+
 def to_categorical(x, n_col=None):
-	# Your code here
-	pass
+    # Your code here
+    pass
+
+
+x = np.array([0, 1, 2, 1, 0])
+output = to_categorical(x)
+print(output)
