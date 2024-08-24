d896da0ec9ed8f5c322072fc28b46b1475ceb716
Srihari Thyagarajan <hari.leo03@gmail.com>
2024-08-24 14:24:23 +0530

    Add logic

--- easy/One_Hot_Encoding_of_Nominal_Values/One_Hot_Encoding_of_Nominal_Values.py
+++ easy/One_Hot_Encoding_of_Nominal_Values/One_Hot_Encoding_of_Nominal_Values.py
@@ -3,6 +3,9 @@ import numpy as np
 
 def to_categorical(x: np.ndarray, n_col=None):
     # Your code here
+    results = [[] for _ in range(max(x) + 1) for _ in range(len(x))]
+    
+    print(results)
     pass
 
 
