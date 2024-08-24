d4f2d841f170fd1fb744cf20302a045b426cc0ef
Srihari Thyagarajan <hari.leo03@gmail.com>
2024-08-24 17:40:59 +0530

    Update logic

--- easy/One_Hot_Encoding_of_Nominal_Values/One_Hot_Encoding_of_Nominal_Values.py
+++ easy/One_Hot_Encoding_of_Nominal_Values/One_Hot_Encoding_of_Nominal_Values.py
@@ -1,14 +1,28 @@
 import numpy as np
 
-
-def to_categorical(x: np.ndarray, n_col=None):
+# # Step 1
+def to_categorical(x, n_col=None):
     # Your code here
-    results = [0 for _ in range(max(x) + 1) for _ in range(len(x))]
-
-    print(results)
-    # for i in results:
-
-    # pass
+    if n_col is None:
+        n_col = x.max() + 1  # Determine the number of columns from the input array
+    
+    one_hot = np.zeros((x.size, n_col), dtype=int)
+    # print(one_hot)
+    one_hot[np.arange(x.size), x] = 1
+    
+    return one_hot
+# # Step 2
+# def to_categorical(x, n_col=None):
+#     # One-hot encoding of nominal values
+#     # If n_col is not provided, determine the number of columns from the input array
+#     if not n_col:
+#         n_col = np.amax(x) + 1
+#     # Initialize a matrix of zeros with shape (number of samples, n_col)
+#     one_hot = np.zeros((x.shape[0], n_col))
+#     # Set the appropriate elements to 1
+#     one_hot[np.arange(x.shape[0]), x] = 1
+#     return one_hot
+    
 
 
 x = np.array([0, 1, 2, 1, 0])
