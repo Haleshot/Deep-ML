c3ca0428ede947012f266c56e476d39bc18b28a1
Srihari Thyagarajan <hari.leo03@gmail.com>
2024-08-25 08:30:05 +0530

    init commit

--- easy/Linear_Regression_Using_Gradient_Descent/Linear_Regression_Using_Gradient_Descent.py
+++ easy/Linear_Regression_Using_Gradient_Descent/Linear_Regression_Using_Gradient_Descent.py
@@ -1,7 +1,18 @@
 import numpy as np
 
-def linear_regression_gradient_descent(X: np.ndarray, y: np.ndarray, alpha: float, iterations: int) -> np.ndarray:
-	# Your code here, make sure to round
-	m, n = X.shape
-	theta = np.zeros((n, 1))
-	return theta
+
+def linear_regression_gradient_descent(
+    X: np.ndarray, y: np.ndarray, alpha: float, iterations: int
+) -> np.ndarray:
+    # Your code here, make sure to round
+    m, n = X.shape
+    theta = np.zeros((n, 1))
+
+    return theta
+
+# Testing
+X = np.array([[1, 1], [1, 2], [1, 3]])
+y = np.array([1, 2, 3])
+alpha = 0.01
+iterations = 1000
+print(linear_regression_gradient_descent(X=X, y=y, alpha=alpha, iterations=iterations))
