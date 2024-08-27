3de8e634a633b5fa1b08b5f3515e685132584c7c
Srihari Thyagarajan <hari.leo03@gmail.com>
2024-08-27 14:22:48 +0530

    Update logic

--- easy/Implement_Precision_Metric/Implement_Precision_Metric.py
+++ easy/Implement_Precision_Metric/Implement_Precision_Metric.py
@@ -14,7 +14,7 @@ import numpy as np
 
 # # Step 2
 def precision(y_true, y_pred):
-    tp, fp = np.sum(y_true == 1 & y_pred == 1), np.sum(y_true == 0 & y_pred == 1)
+    tp, fp = np.sum((y_true == 1) & (y_pred == 1)), np.sum((y_true == 0) & (y_pred == 1))
     return tp / (tp + fp) if (tp + fp) > 0 else 0.0
 
 
