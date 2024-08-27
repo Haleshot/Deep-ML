6ceee33fb478d33bb1c41cca9c22acd8fd0aac9c
Srihari Thyagarajan <hari.leo03@gmail.com>
2024-08-27 14:12:14 +0530

    Format using `black`

--- easy/Implement_Precision_Metric/Implement_Precision_Metric.py
+++ easy/Implement_Precision_Metric/Implement_Precision_Metric.py
@@ -4,3 +4,9 @@ import numpy as np
 def precision(y_true, y_pred):
     # Your code here
     pass
+
+y_true = np.array([1, 0, 1, 1, 0, 1])
+y_pred = np.array([1, 0, 1, 0, 0, 1])
+
+result = precision(y_true, y_pred)
+print(result)
