613a424c58a5041d30445b7880cbbf725348fd9e
Srihari Thyagarajan <hari.leo03@gmail.com>
2024-08-27 14:17:49 +0530

    Add logic

--- easy/Implement_Precision_Metric/Implement_Precision_Metric.py
+++ easy/Implement_Precision_Metric/Implement_Precision_Metric.py
@@ -1,9 +1,15 @@
 import numpy as np
 
-
+# # Step 1
 def precision(y_true, y_pred):
     # Your code here
-    pass
+    tp, fp = 0, 0
+    for i in range(len(y_pred)):
+        if (y_pred[i] == y_true[i]) and y_true[i] == 1:
+            tp += 1
+        elif (y_pred[i] != y_true[i]) and y_pred[i] == 1:
+            fp += 1
+    return tp/(tp + fp)
 
 
 y_true = np.array([1, 0, 1, 1, 0, 1])
