6975246a4e33194ce09ea0a64b9d137e672315d3
Srihari Thyagarajan <hari.leo03@gmail.com>
2024-08-29 21:01:57 +0530

    Init commit

--- easy/Single_Neuron/Single_Neuron.py
+++ easy/Single_Neuron/Single_Neuron.py
@@ -6,3 +6,10 @@ def single_neuron_model(
 ) -> (list[float], float):
     # Your code here
     return probabilities, mse
+
+
+features = [[0.5, 1.0], [-1.5, -2.0], [2.0, 1.5]]
+labels = [0, 1, 0]
+weights = [0.7, -0.4]
+bias = -0.1
+print(single_neuron_model(features=features, labels=labels, weights=weights, bias=bias))
