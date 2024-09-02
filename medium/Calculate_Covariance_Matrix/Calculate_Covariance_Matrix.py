44dd3c72ac9505e38682b9b665b376f194a6921f
Srihari Thyagarajan <hari.leo03@gmail.com>
2024-09-02 22:36:08 +0530

    Format using `black`

--- medium/Calculate_Covariance_Matrix/Calculate_Covariance_Matrix.py
+++ medium/Calculate_Covariance_Matrix/Calculate_Covariance_Matrix.py
@@ -1,7 +1,7 @@
 def calculate_covariance_matrix(vectors: list[list[float]]) -> list[list[float]]:
-    
+
     return covariance_matrix
 
 
 vectors = [[1, 2, 3], [4, 5, 6]]
-print(calculate_covariance_matrix(vectors=vectors)) # type: ignore
+print(calculate_covariance_matrix(vectors=vectors))  # type: ignore
