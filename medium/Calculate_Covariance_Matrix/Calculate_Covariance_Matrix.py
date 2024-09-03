36b6af9b32793edd0b3a98555b885f11a8651f59
Srihari Thyagarajan <hari.leo03@gmail.com>
2024-09-03 07:26:06 +0530

    Format using `black`

--- medium/Calculate_Covariance_Matrix/Calculate_Covariance_Matrix.py
+++ medium/Calculate_Covariance_Matrix/Calculate_Covariance_Matrix.py
@@ -1,11 +1,11 @@
 def calculate_covariance_matrix(vectors: list[list[float]]) -> list[list[float]]:
     covariance_matrix = [[] for _ in range(len(vectors))]
     for i in vectors:
-        meanX = sum(i)/len(i)
+        meanX = sum(i) / len(i)
         j = vectors[vectors.index(i) + 1]
-        meanY = sum(j)/len(j)
+        meanY = sum(j) / len(j)
         covariance_matrix += [meanX + meanY]
-    covariance_matrix = covariance_matrix/len(vectors) - 1
+    covariance_matrix = covariance_matrix / len(vectors) - 1
 
     return covariance_matrix
 
