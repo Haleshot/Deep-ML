05a3dca155bd8191fc6505cbc2c9e3be9b63b21a
Srihari Thyagarajan <hari.leo03@gmail.com>
2024-09-04 17:38:00 +0530

    Update logic

--- medium/Solve Linear_Equations_using_Jacobi_Method/Solve Linear_Equations_using_Jacobi_Method.py
+++ medium/Solve Linear_Equations_using_Jacobi_Method/Solve Linear_Equations_using_Jacobi_Method.py
@@ -3,8 +3,16 @@ import numpy as np
 
 def solve_jacobi(A: np.ndarray, b: np.ndarray, n: int) -> list:
     aii = np.diag(A).tolist()
+    # print(A.shape[0])
+
     # print(aii)
-    # return x
+    m = np.array(A).shape[0]
+    strided = np.lib.stride_tricks.as_strided
+    s0, s1 = np.array(A).strides
+    aij = (strided(np.array(A).ravel()[1:], shape=(m-1,m), strides=(s0+s1,s1)).reshape(m,-1))
+    print(aij)
+
+    
 
 
 A = [[5, -2, 3], [-3, 9, 1], [2, -1, -7]]
