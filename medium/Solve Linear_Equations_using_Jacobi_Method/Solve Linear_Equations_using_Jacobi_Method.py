da22bb8e8615d5296867a133aee8f322c24eb99c
Srihari Thyagarajan <hari.leo03@gmail.com>
2024-09-04 17:20:55 +0530

    Add logic

--- medium/Solve Linear_Equations_using_Jacobi_Method/Solve Linear_Equations_using_Jacobi_Method.py
+++ medium/Solve Linear_Equations_using_Jacobi_Method/Solve Linear_Equations_using_Jacobi_Method.py
@@ -2,9 +2,12 @@ import numpy as np
 
 
 def solve_jacobi(A: np.ndarray, b: np.ndarray, n: int) -> list:
-    return x
+    aii = np.diag(A).tolist()
+    # print(aii)
+    # return x
 
 
 A = [[5, -2, 3], [-3, 9, 1], [2, -1, -7]]
 b = [-1, 2, 3]
 n = 2
+print(solve_jacobi(A=A, b=b, n=n))
