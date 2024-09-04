da6ba444e946110e82978bd45dceec7b4a4dd5e7
Srihari Thyagarajan <hari.leo03@gmail.com>
2024-09-04 17:38:23 +0530

    Format using `black`

--- medium/Solve Linear_Equations_using_Jacobi_Method/Solve Linear_Equations_using_Jacobi_Method.py
+++ medium/Solve Linear_Equations_using_Jacobi_Method/Solve Linear_Equations_using_Jacobi_Method.py
@@ -9,13 +9,13 @@ def solve_jacobi(A: np.ndarray, b: np.ndarray, n: int) -> list:
     m = np.array(A).shape[0]
     strided = np.lib.stride_tricks.as_strided
     s0, s1 = np.array(A).strides
-    aij = (strided(np.array(A).ravel()[1:], shape=(m-1,m), strides=(s0+s1,s1)).reshape(m,-1))
+    aij = strided(
+        np.array(A).ravel()[1:], shape=(m - 1, m), strides=(s0 + s1, s1)
+    ).reshape(m, -1)
     print(aij)
-
-    
 
 
 A = [[5, -2, 3], [-3, 9, 1], [2, -1, -7]]
 b = [-1, 2, 3]
 n = 2
-print(solve_jacobi(A=A, b=b, n=n)) # type: ignore
+print(solve_jacobi(A=A, b=b, n=n))  # type: ignore
