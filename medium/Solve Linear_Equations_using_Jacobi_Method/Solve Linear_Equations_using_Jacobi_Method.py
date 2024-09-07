e9d2229dd34e0da75b9523c40fc18271d72c1a23
Srihari Thyagarajan <hari.leo03@gmail.com>
2024-09-07 11:37:03 +0530

    Update logic

--- medium/Solve Linear_Equations_using_Jacobi_Method/Solve Linear_Equations_using_Jacobi_Method.py
+++ medium/Solve Linear_Equations_using_Jacobi_Method/Solve Linear_Equations_using_Jacobi_Method.py
@@ -24,6 +24,17 @@ def solve_jacobi(A: np.ndarray, b: np.ndarray, n: int) -> list:
     
     return x
 
+# # Step 2
+# def solve_jacobi(A: np.ndarray, b: np.ndarray, n: int) -> list:
+#     d_a = np.diag(A)
+#     nda = A - np.diag(d_a)
+#     x = np.zeros(len(b))
+#     x_hold = np.zeros(len(b))
+#     for _ in range(n):
+#         for i in range(len(A)):
+#             x_hold[i] = (1/d_a[i]) * (b[i] - sum(nda[i]*x))
+#         x = x_hold.copy()
+#     return np.round(x,4).tolist()
 
 A = [[5, -2, 3], [-3, 9, 1], [2, -1, -7]]
 b = [-1, 2, 3]
