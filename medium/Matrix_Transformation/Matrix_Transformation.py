bc069dec2bbe6523abf3bcb5a0c70ceab213a561
Srihari Thyagarajan <hari.leo03@gmail.com>
2024-09-09 15:23:10 +0530

    Update logic

--- medium/Matrix_Transformation/Matrix_Transformation.py
+++ medium/Matrix_Transformation/Matrix_Transformation.py
@@ -4,6 +4,11 @@ import numpy as np
 def transform_matrix(
     A: list[list[int | float]], T: list[list[int | float]], S: list[list[int | float]]
 ) -> list[list[int | float]]:
+
+    if np.linalg.det(T) == 0 or np.linalg.det(S) == 0:
+        raise ValueError(
+            "The determinant/s of the matrice/s equals 0; hence not invertible"
+        )
     transformed_matrix = np.linalg.inv(T) @ A @ S
     return transformed_matrix # type: ignore
 
@@ -11,4 +16,4 @@ def transform_matrix(
 A = [[1, 2], [3, 4]]
 T = [[2, 0], [0, 2]]
 S = [[1, 1], [0, 1]]
-print(transform_matrix(A=A, T=T, S=S)) # type: ignore
+print(transform_matrix(A=A, T=T, S=S))  # type: ignore
