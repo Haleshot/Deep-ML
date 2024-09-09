835ac458fc3ace5e6de3f277f99febdb06b03c5b
Srihari Thyagarajan <hari.leo03@gmail.com>
2024-09-09 15:14:37 +0530

    Update logic

--- medium/Matrix_Transformation/Matrix_Transformation.py
+++ medium/Matrix_Transformation/Matrix_Transformation.py
@@ -5,10 +5,10 @@ def transform_matrix(
     A: list[list[int | float]], T: list[list[int | float]], S: list[list[int | float]]
 ) -> list[list[int | float]]:
     transformed_matrix = np.linalg.inv(T) @ A @ S
-    return transformed_matrix
+    return transformed_matrix # type: ignore
 
 
 A = [[1, 2], [3, 4]]
 T = [[2, 0], [0, 2]]
 S = [[1, 1], [0, 1]]
-print(transform_matrix(A=A, T=T, S=S))
+print(transform_matrix(A=A, T=T, S=S)) # type: ignore
