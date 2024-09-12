6658741371ebf202636be82d29344df54d6e6840
Srihari Thyagarajan <hari.leo03@gmail.com>
2024-09-12 20:02:34 +0530

    Format using `black`

--- medium/K-Means_Clustering/K-Means_Clustering.py
+++ medium/K-Means_Clustering/K-Means_Clustering.py
@@ -10,7 +10,9 @@ def k_means_clustering(
 ) -> list[tuple[float, float]]:
     # Your code here
     def euc_dist(x1, x2):
-        return math.sqrt(((x2[1] - x1[1]) ** 2 + (x2[0] - x1[0]) ** 2)) # Euclidean distance formula
+        return math.sqrt(
+            ((x2[1] - x1[1]) ** 2 + (x2[0] - x1[0]) ** 2)
+        )  # Euclidean distance formula
 
     final_centroids = initial_centroids
     # print(final_centroids)
@@ -24,4 +26,4 @@ points = [(1, 2), (1, 4), (1, 0), (10, 2), (10, 4), (10, 0)]
 k = 2
 initial_centroids = [(1, 1), (10, 1)]
 max_iterations = 10
-print(k_means_clustering(points=points, k=k, initial_centroids=initial_centroids, max_iterations=max_iterations)) # type: ignore
+print(k_means_clustering(points=points, k=k, initial_centroids=initial_centroids, max_iterations=max_iterations))  # type: ignore
