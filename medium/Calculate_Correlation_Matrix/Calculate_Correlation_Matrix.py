e5697d71b09dde39d21e90eba0f692dde94649d3
Srihari Thyagarajan <hari.leo03@gmail.com>
2024-11-01 23:06:35 +0530

    Update logic

--- medium/Calculate_Correlation_Matrix/Calculate_Correlation_Matrix.py
+++ medium/Calculate_Correlation_Matrix/Calculate_Correlation_Matrix.py
@@ -1,9 +1,14 @@
 import numpy as np
 
 
-def calculate_correlation_matrix(X : np.ndarray, Y=None):
+def calculate_correlation_matrix(X, Y=None):
     # Your code here
-    pass
+    if Y:
+        pass
+    cov = np.cov(X)
+    std = np.std(X)
+    corr = cov / std * std
+    return corr
 
 
 X = np.array([[1, 2], [3, 4], [5, 6]])
